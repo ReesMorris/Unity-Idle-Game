dda57ef8e67d9aa97517eaed8ad7a073bed778d7
Rees Morris <reesmorriscontact@gmail.com>
2019-01-05 11:31:02 +0000

    Began fixing the money issues
    - Money doesn't work as expected

---
+++ Assets/Buyable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buyable : MonoBehaviour {
+
+    public string name;
+    public Button buttonUI;
+    public Text costUI;
+    public float cost;
+    public float upgradeCostIncrement;
+
+    int owned = 0;
+    MoneyManager moneyManager;
+
+	void Start () {
+        moneyManager = MoneyManager.Instance;
+        buttonUI.onClick.AddListener(OnButtonClick);
+
+        UpdateUI();
+
+    }
+
+    void OnButtonClick() {
+
+        if (moneyManager.CanAffordPurchase(cost))
+            OnPurchase();
+    }
+
+    void OnPurchase() {
+        owned++;
+
+        cost *= upgradeCostIncrement;
+
+        UpdateUI();
+    }
+
+    void UpdateUI() {
+        costUI.text = moneyManager.GetFormattedMoney(cost);
+    }
+}
--- Assets/MoneyManager.cs
+++ Assets/MoneyManager.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 
 public class MoneyManager : MonoBehaviour {
 
+    // Define the instance so we can use it easily from other scripts
+    public static MoneyManager Instance;
+
     // Public Variables
     [Header("UI Elements")]
     [Tooltip("The UI element of the text which displays our money")] public Text moneyText;
 
     [Header("Config")]
+    [Tooltip("The amount of money the player starts the game with")] public float startingMoney;
     [Tooltip("The currency icon used throughout the game")] public string currencyIcon;
     [Tooltip("The minimum length of a number's digits before the first name (ie. Thousands) is displayed. Must be divisible by 3")] public int firstNameLength = 3;
     [Tooltip("The number of decimal places to display")] public int decimalPlaces = 3;
@@ -17,6 +21,17 @@ public class MoneyManager : MonoBehaviour {
 
     // Private Variables
     float money;
+
+    // Called when the game begins
+    void Start() {
+        // Set this to be an instance of itself, so we can call this script from others
+        Instance = this;
+
+        // Add the starting funds
+        AddMoney(startingMoney);
+    }
+
+
 
     // Can be called by other scripts to add money to the user's balance
     public void AddMoney(float amount) {
@@ -27,6 +42,8 @@ public class MoneyManager : MonoBehaviour {
         // Call a function to say that our balance has changed
         OnBalanceChanged();
     }
+
+
 
     // Reduce the amount of money the player has. This function will not check to see if the user has enough money
     public void ReduceMoney(float amount) {
@@ -45,84 +62,73 @@ public class MoneyManager : MonoBehaviour {
         OnBalanceChanged();
     }
 
+
+
     // Returns true or false depending on whether or not the player has enough money
-    bool CanAffordPurchase(float cost) {
+    public bool CanAffordPurchase(float cost) {
 
         // Returns whether the player's balance is higher than the cost
         return money > cost;
     }
 
+
+
     // Called when the balance of the user has been changed
     void OnBalanceChanged() {
 
-        // Update the UI text to show our new balance
-        SetDisplayMoneyText();
+        moneyText.text = GetStringMoney(money);
     }
 
-    // Change the text of the money to show how much we have
-    void SetDisplayMoneyText() {
 
-        // Grab a string version of the amount of money we have, so that we can read the number of digits it has
-        string stringMoney = money.ToString("F0");
-        string displayMoney = stringMoney;
 
+    // Returns the balance as a string
+    public string GetStringMoney(float money) {
+
+        // Convert the money value to a string with zero decimal places
+        return money.ToString("F0");
+    }
+
+
+
+    // Returns the money value formatted with the currency character, decimal place, amount, and the name of the number (if applicable)
+    public string GetFormattedMoney(float money) {
+
+        // Convert the money to a string
+        string stringMoney = GetStringMoney(money);
+
+        /*
         // Calculate how many digits come before the decmial place, which will be at most 3 digits
-        int digitsBeforeDecimal = displayMoney.Length % 3;
+        int digitsBeforeDecimal = stringMoney.Length % 3;
         if (digitsBeforeDecimal == 0) digitsBeforeDecimal = 3;
+        */
 
+        /*
         // Do we want to display decimal places?
         if (decimalPlaces > 0) {
 
             // Only show a decimal place if there is a word for the number amount; otherwise it looks weird
-            if (displayMoney.Length > firstNameLength) {
+            if (stringMoney.Length > firstNameLength) {
 
                 // Insert a decimal place into the string based on the amount of digits before the decimal, calculated above
-                displayMoney = displayMoney.Insert(digitsBeforeDecimal, ".");
+                stringMoney = stringMoney.Insert(digitsBeforeDecimal, ".");
             }
 
             // Check to see if there are enough digits in the number to actually display a decimal place
-            if (displayMoney.Length - digitsBeforeDecimal > decimalPlaces) {
+            if (stringMoney.Length - digitsBeforeDecimal > decimalPlaces) {
 
                 // Reduce the string to be a length equal to the number of places it has before the decimal, as well as the decimal places (plus the decimal itself)
-                displayMoney = displayMoney.Substring(0, digitsBeforeDecimal + decimalPlaces + 1);
+                stringMoney = stringMoney.Substring(0, digitsBeforeDecimal + decimalPlaces + 1);
             }
         }
+        */
 
-        // Update the UI text to show the new amount of money we have
-        moneyText.text = displayMoney + " " + GetNumberName(stringMoney.Length);
-    }
-
-    // Will return the name of the number the integer is connected to
-    string GetNumberName(int numberLength) {
-
-        // If we don't want to show a name for this number, just return an empty string
-        if (numberLength < firstNameLength) return "";
-
-        // The number should have a name, let's return it
-        else {
-
-            // The string of potential names (to be moved to a .txt file in future)
-            string[] names = { "Hundred", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion" };
-
-            // Grab the index value based on the length of the number, as well as the offset we start from
-            int index = Mathf.CeilToInt((float)numberLength / 3f) - Mathf.CeilToInt((float)firstNameLength / 3f);
-
-            // If the name exists in the array (the number is small enough) then return the number
-            if (names.Length > index) return names[index];
-
-            // If the number doesn't exist in the array for whatever reason, return a generic phrase
-            else return "a lot";
-        }
-    }
-
-    // Returns the money value formatted with the currency character, amount, and the name of the number (if applicable)
-    public string GetFormattedMoney(string stringMoney) {
-
+        /*
         // Add the currency icon
         stringMoney.Insert(0, currencyIcon);
+        */
 
         // Add the number name
-        stringMoney += " " + GetNumberName(stringMoney.Length);
+            //stringMoney += " " + GetNumberName(stringMoney.Length);
 
         return stringMoney;
     }
